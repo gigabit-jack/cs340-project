1a5a2e5f369d2a92eec9b8b8ee493077b33a9846
gigabit-jack <65245638+gigabit-jack@users.noreply.github.com>
2025-11-19 22:10:39 +1300

    specify db connection params using local .env file

---
+++ project/database/db-connector.js
@@ -0,0 +1,17 @@
+// Get an instance of mysql we can use in the app
+let mysql = require('mysql2')
+
+require('dotenv').config()
+
+// Create a 'connection pool' using the provided credentials
+const pool = mysql.createPool({
+    waitForConnections: true,
+    connectionLimit   : 10,
+    host              : process.env.DB_HOST,
+    user              : process.env.DB_USER,
+    password          : process.env.DB_PASS,
+    database          : process.env.DB_DB
+}).promise(); // This makes it so we can use async / await rather than callbacks
+
+// Export it for use in our application
+module.exports = pool;
