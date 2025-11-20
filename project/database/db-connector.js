206018842a6682bdb2bfc73ef9b3a4d525d36e07
gigabit-jack <65245638+gigabit-jack@users.noreply.github.com>
2025-11-21 12:51:55 +1300

    added port variable for DB connection; uses dotenv with .env file

--- project/database/db-connector.js
+++ project/database/db-connector.js
@@ -8,6 +8,7 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit   : 10,
     host              : process.env.DB_HOST,
+    port              : process.env.DB_PORT,
     user              : process.env.DB_USER,
     password          : process.env.DB_PASS,
     database          : process.env.DB_DB
